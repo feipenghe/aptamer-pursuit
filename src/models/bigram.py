2c0019ff7594357d2d4e3fa8230568a93a654a31
Aishwarya Mandyam <aishwarya_m@live.com>
2019-10-07 19:38:26 -0700

    Adding initial experiment files

---
+++ src/models/bigram.py
@@ -0,0 +1,19 @@
+# A bigram predicts the likelihood of the current element based on the previous element.
+
+# Construct a training dataset:
+corpus = ["He said thank you.", "He said bye as he walked through the door.", "He went to San "
+        "Diego.", "San Diego has nice weather.", "It is raining in San Francisco."]
+
+# Probabilities: (Count (wp wn))/(Count (wp))
+
+
+
+# Write the model
+
+
+# Train the model
+
+
+# Test the model
+
+
