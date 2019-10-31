ae249160aada5d81d14453f99f0b59732c7d1e1e
Aishwarya Mandyam <aishwarya_m@live.com>
2019-10-30 21:01:21 -0700

    bigram model is working

--- src/models/bigram.py
+++ src/models/bigram.py
@@ -1,6 +1,7 @@
 # A bigram predicts the likelihood of the current element based on the previous element.
 import json
 import nltk
+import random
 
 # Construct a training dataset:
 dataset_file = "../data/bigram_dataset.json"
@@ -10,21 +11,36 @@ with open(dataset_file, 'r') as f:
 
 # Generate bigrams for each allele in the dataset
 for allele in bigram_dataset:
+
     sentences = []
     proteins = bigram_dataset[allele]
     for p in proteins:
-        sentences.append(list(p))
+        sentences.append(p)
 
     # Generates the bigrams for this particular peptide set for this allele
-    bigram_list = []
+    bigrams = {}
     for sentence in sentences:
-        bigram_list.extend(list(nltk.bigrams(sentence)))
+        for i in range(len(sentence) - 2):
+            seq = str(sentence[i:i+2])
+            if seq not in bigrams.keys():
+                bigrams[seq] = []
+            bigrams[seq].append(sentence[i+2])
 
-    print(sentences)
-    break
+    # Generate the best 8 amino acid protein sequence
+    # with a given tart letter
+    start = sentences[0][0:2]
+    output = start
+    for i in range(6):
+        if start not in bigrams.keys():
+            break
+        possible_chars = bigrams[start]
+        next_char = possible_chars[random.randrange(len(possible_chars))]
+        output += next_char
+        start = output[len(output)-2:len(output)]
+
+    print("Allele: " + str(allele) + ", Predicted Bigram Protein: " + str(output))
 
 
-# Generate using the first couple of words from dataset (word is a protein)
-# https://stackabuse.com/python-for-nlp-developing-an-automatic-text-filler-using-n-grams/ --> this is a useful link to understand
+
 
 
