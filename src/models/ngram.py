5afb0f6d80d783c4709e70ce0bb6e1566ae0f96c
Aishwarya Mandyam <aishwarya_m@live.com>
2019-11-10 22:02:43 -0800

    saving feature locality work

--- src/models/ngram.py
+++ src/models/ngram.py
@@ -3,6 +3,9 @@ import json
 import random
 import numpy as np
 from sklearn import linear_model, metrics
+from scipy import stats
+import random
+import matplotlib.pyplot as plt
 
 # Construct a training dataset:
 dataset_file = "../data/ngram_dataset.json"
@@ -17,9 +20,10 @@ regularization_params = [0, 0.02, 0.03, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
 '''
 Generate and evaluate the performance of features to predict whether a protein will bind to a specific allele. 
 '''
-def predict_proteins(k, d, regularization_params):
+def predict_proteins(k, d):
     total_train_MSE = 0.0
     total_test_MSE = 0.0
+
     for allele in ngram_dataset:
         # These are protein/binding affinity pairs
         proteins = ngram_dataset[allele]
@@ -28,6 +32,10 @@ def predict_proteins(k, d, regularization_params):
         num_samples = len(proteins)
         if num_samples < 20:
             continue
+
+        # Randomly select training and test
+        random.shuffle(proteins)
+
         training_set = proteins[:int(num_samples*0.8)]
         testing_set = proteins[int(num_samples*0.8):]
 
@@ -36,6 +44,7 @@ def predict_proteins(k, d, regularization_params):
         train_y = [np.log10(float(b)) for (p, b) in training_set]
         testing_peptides = [p for (p,b) in testing_set]
         test_y = [np.log10(float(b)) for (p, b) in testing_set]
+        # Features --> Quartile
         features = []
         for i in range(d):
             # Find a random sequence in the training set
@@ -43,10 +52,19 @@ def predict_proteins(k, d, regularization_params):
 
             # Find a random subsection of k elements from this sequence
             start = random.randint(0, len(seq) - k)
+            quartile_pctg = (start + 1)/float(len(seq))
+            if quartile_pctg <= 0.25:
+                quartile = 1
+            elif quartile_pctg > 0.25 and quartile_pctg <= 0.5:
+                quartile = 2
+            elif quartile_pctg > 0.5 and quartile_pctg <= 0.75:
+                quartile = 3
+            else:
+                quartile = 4
+
             feature = seq[start:start+k]
 
-            # Add it to the list of features here
-            features.append(feature)
+            features.append((feature, quartile))
 
         # Generate train and test matrices
         train_features = np.zeros((len(training_peptides), d))
@@ -56,7 +74,9 @@ def predict_proteins(k, d, regularization_params):
         for i in range(len(training_peptides)):
             sequence = training_peptides[i]
             for j in range(len(features)):
-                feature = features[j]
+                feature, quartile = features[j]
+                # If it's in the correct quartile, it's 1
+                
                 train_features[i, j] = 1 if str(feature) in str(sequence) else 0
                 if train_features[i, j] == 1:
                     train_ones += 1
@@ -68,7 +88,6 @@ def predict_proteins(k, d, regularization_params):
                 test_features[i, j] = 1 if feature in sequence else 0
 
         # Use a linear model here to calculate the best parameters for the linear regression model
-
         model = linear_model.Ridge()
         model.fit(train_features, train_y)
 
@@ -79,13 +98,14 @@ def predict_proteins(k, d, regularization_params):
         test_predict = model.predict(test_features)
 
         gt_test = test_y
+
         test_mse = metrics.mean_squared_error(test_predict, gt_test)
 
         total_train_MSE += train_mse
         total_test_MSE += test_mse
 
         #print("Allele: " + str(allele) + " Train MSE: " + str(train_mse) + " Test MSE: " + str(test_mse))
-    print("D: " + str(d) + " K: " + str(k))
+
     return (d, k, total_train_MSE, total_test_MSE)
 
 '''
@@ -111,8 +131,8 @@ def run_experiments():
     # k --> (d, total_train_mse, total_test_mse)
     experiments = {}
     for k in range(2, 7):
-        for d in range(100, 1000, 100):
-            d, k, total_train_mse, total_test_mse = predict_proteins(k, d, regularization_params)
+        for d in range(100, 1100, 100):
+            d, k, total_train_mse, total_test_mse = predict_proteins(k, d)
             if k not in experiments:
                 experiments[k] = []
             experiments[k].append((d, total_train_mse, total_test_mse))
@@ -122,8 +142,20 @@ def run_experiments():
         json.dump(experiments, f)
 
 
-run_experiments()
-#predict_proteins(k=3, d=400, regularization_params=regularization_params)
+predict_proteins(2, 100)
+#run_experiments()
+#predict_proteins(k=4, d=1000)
+
+#gt, predict= predict_proteins(k=4, d=1000)
+#r = stats.pearsonr(gt, list(predict))
+#print("R squared: ", pow(r[0], 2))
+#with open("../../experiments/HLA-A*02:01-predict.json", 'w') as f:
+#    json.dump(list(predict), f)
+#with open("../../experiments/HLA-A*02:01-true.json", 'w') as f:
+#    json.dump(list(gt), f)
+
+
+#run_experiments()
 
 
 
